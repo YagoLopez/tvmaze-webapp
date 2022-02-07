3d57bdc2915427a97ac5907f1224ec40d76c427b
Yago Lopez <yago.lopez@gmail.com>
2022-02-07 04:13:10 +0100

    Fix typo in helper text

--- apps/tvmaze-webapp/constants/index.ts
+++ apps/tvmaze-webapp/constants/index.ts
@@ -3,6 +3,6 @@ const MIN_LENGTH = 4
 export const CONST = {
   MIN_LENGTH,
   HELPER_TEXT: "Press 'Enter' key or 'Search' button",
-  HELPER_TEXT_ERROR: `(Minimun text length: ${MIN_LENGTH})`,
+  HELPER_TEXT_ERROR: `(Minimum text length: ${MIN_LENGTH})`,
   INPUT_LABEL: 'Enter text',
 }
